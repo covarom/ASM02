94d99e94de2c8472a69f124eba8d5a52ca906b4a
quocanh0508 <89030747+quocanh0508@users.noreply.github.com>
2022-08-24 17:36:22 +0700

    Finish project

--- SaleManagementWinApp/ReportFrom.cs
+++ SaleManagementWinApp/ReportFrom.cs
@@ -15,7 +15,50 @@ namespace SaleManagementWinApp
 {
     public partial class ReportFrom : Form
     {
-        
+        IOrderDetailsReponsity orderDetailsReponsity = new OrderDetailsReponsity();
+        IOrderReponsity orderReponsity = new OrderReponsity();
+
+        BindingSource source;
+        public ReportFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetReport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime startdate = DateTime.Parse(dtpStartDate.Text);
+                DateTime endDate = DateTime.Parse(dtpEndStart.Text);
+
+                List<ReportList> reportList = new List<ReportList>();
+
+                List<Order> orderList = orderReponsity.GetReport(startdate, endDate).ToList();
+
+                foreach (var order in orderList)
+                {
+                    ReportList reportLists = new ReportList();
+
+                    reportList.Add(new ReportList()
+                    {
+                        orderID = order.OrderId,
+                        total = orderDetailsReponsity.TotalOfOrderDetail(order.OrderId)
+                    });
+
+                }
+
+                source = new BindingSource();
+                source.DataSource = reportList;
+                dvgDataReport.DataSource = source;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Get report .. " + ex.Message);
+            }
+        }
     }
 
 }
